eef49dedc3fbfcd33af469fd0544f3f47d30241e
Andrey <andryusha-belousov@inbox.ru>
2023-11-08 21:44:32 +0500

    7

--- Tema7/sam75.py
+++ Tema7/sam75.py
@@ -1,12 +1,6 @@
-def find(string_to_find):
-    with open('sam71input.txt', 'r', encoding='utf-8') as file:
-        text = file.read()
-        text = text.lower()
-        count = text.count(string_to_find.lower())
-        return count
-
-
 string = input("Введите текст для поиска: ")
-word_count = find(string)
-
-print(f"'{string}' встречается в тексте {word_count} раз(а).")
+with open('sam71input.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
+    text = text.lower()
+    count = text.count(string.lower())
+    print(f"'{string}' встречается в тексте {count} раз(а).")
