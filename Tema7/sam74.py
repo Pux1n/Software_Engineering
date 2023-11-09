b6415dd1dbc5acc618e136b080fe8eae1621728f
Andrey <andryusha-belousov@inbox.ru>
2023-11-09 21:36:25 +0500

    t8

--- Tema7/sam74.py
+++
@@ -1,8 +0,0 @@
-import re
-
-sentence = 'Hello, world! Python IS the programming language of thE future. My EMAIL is.... \nPYTHON is awesome!!!!'
-with open('input.txt', 'r') as f:
-    ban_words = f.readline().split()
-    for word in ban_words:
-        sentence = re.sub(word, len(word)*'*', sentence, flags=re.IGNORECASE)
-print(sentence)
--- Tema7/sam75.py
+++
@@ -1,6 +0,0 @@
-string = input("Введите текст для поиска: ")
-with open('sam71input.txt', 'r', encoding='utf-8') as file:
-    text = file.read()
-    text = text.lower()
-    count = text.count(string.lower())
-    print(f"'{string}' встречается в тексте {count} раз(а).")
